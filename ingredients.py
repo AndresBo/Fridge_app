756ded28298c29d34117c4fdcfc05036ab8d76a5
AndresBo <anndresfborrero@gmail.com>
2022-12-09 22:42:06 +1000

    added getters and setters to Ingredient class

--- ingredients.py
+++ ingredients.py
@@ -1,16 +1,20 @@
-import
+from return_integer import get_int
 
 #define a ingredient:
 class Ingredient:
     def __init__(self, name, count):
-        #name of ingredient:
         self.name = name
-        #count of ingredient:        
         self.count = count
 
-        def get_ingredient_count(self):    
-            return self(count)
+    def get_ingredient_count(self):    
+        return self.count
 
-        def set_ingredient_count(self, new_count):
-                   
-               
+    def set_ingredient_count(self, new_count):
+        new_count = get_int()    
+        self.count = new_count       
+
+banana = Ingredient("banana", 1)               
+
+print(banana.get_ingredient_count())
+banana.set_ingredient_count("a")
+print(banana.get_ingredient_count())
--- test/acceptance_test.py
+++ test/acceptance_test.py
@@ -1,7 +1,13 @@
-class TestingIngredients:
-    def test_ingredients_count():
-        items = {
-          "bananas":1,
-          "pears":4,
-        }             
-    items_count
+import pytest
+from return_integer import get_int
+# class TestingIngredients:
+#     def test_ingredients_count():
+#         items = {
+#           "bananas":1,
+#           "pears":4,
+#         }             
+#     items_count
+
+def test_get_int():
+    with pytest.raises(ValueError):
+        get_int()
