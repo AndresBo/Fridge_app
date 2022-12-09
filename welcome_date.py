911e46157bd2bc137d5ddbdb50c50e716f825c5d
AndresBo <anndresfborrero@gmail.com>
2022-12-09 20:50:08 +1000

    modified welcome_date.py

--- welcome_date.py
+++ welcome_date.py
@@ -1,10 +1,11 @@
 import datetime as dt
 
 # welcome message current date:
-now = dt.datetime.now()
-day_number = (now.strftime("%d"))
-day_name = (now.strftime("%A"))
-month_name = (now.strftime("%B"))
-year = (now.strftime("%Y"))
+def welcome_date():
+    now = dt.datetime.now()
+    day_number = (now.strftime("%d"))
+    day_name = (now.strftime("%A"))
+    month_name = (now.strftime("%B"))
+    year = (now.strftime("%Y"))
 
-print(f"Today is {day_name} the {day_number} of {month_name}, {year}")
+    return (f"Today is {day_name} the {day_number} of {month_name}, {year}")
