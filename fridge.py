ff4eec7625223619567ca43c60fea84dda65b9c4
AndresBo <anndresfborrero@gmail.com>
2022-12-10 21:55:36 +1000

    started building start_menu

--- fridge.py
+++ fridge.py
@@ -1,3 +1,10 @@
 from welcome_date import welcome_date
 
-print(welcome_date())
+def start_menu():
+    print("\n------------ Welcome to Fridge ------------")
+    print(welcome_date())
+    print("\nPlease select from the following options by entering the number:")
+    print("1. List ingredients in fridge")
+
+
+start_menu()
