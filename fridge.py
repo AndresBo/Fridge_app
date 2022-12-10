726bad01383b005dd32839edd780c77c1d95a3b5
AndresBo <anndresfborrero@gmail.com>
2022-12-10 22:32:04 +1000

    implemented start menu within a while loop

--- fridge.py
+++ fridge.py
@@ -1,10 +1,36 @@
+from os import system
 from welcome_date import welcome_date
 
 def start_menu():
-    print("\n------------ Welcome to Fridge ------------")
+    print("\n------------ Welcome to FRIDGE ------------")
     print(welcome_date())
     print("\nPlease select from the following options by entering the number:")
-    print("1. List ingredients in fridge")
+    print("1. List of foods in fridge")
+    print("2. Enter new food")
+    print("3. Remove food")
+    print("4. Query food amount")
+    print("5. Exit")
+    selection = input("\nPlease enter your selection: ")
+    return selection
+    
+selection = ""
+while selection != "5":
+    system('clear')
+    selection = start_menu()
+    system('clear')
+    if selection == "1": # list foods
+        print("list of food")
+    elif selection == "2": # enter new food
+        print("enter new food")
+    elif selection == "3": # remove food
+        print("remove food")
+    elif selection == "4": # query food amount
+        print("query food")
+    elif selection == "5": #exit
+        continue
+    else:
+        print("Invalid input")
+    input("\nPress enter to continue")
+    system('clear')
 
-
-start_menu()
+print("bye")
