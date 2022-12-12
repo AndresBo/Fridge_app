e000f647deeb09e95c38848cde9c72809728f286
AndresBo <anndresfborrero@gmail.com>
2022-12-12 13:13:21 +1000

    renaming files for clarity

---
+++ build_set_food_names.py
@@ -0,0 +1,14 @@
+import csv
+from return_lower_case_string import get_string 
+
+# csv -> set
+# build a set of current names in food_list.csv
+def build_set_name():
+    names_set = set()
+    with open('food_list.csv') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            names_set.add(f"{row['food']}")
+    return names_set
+
+
--- new_ingredient.py
+++ new_ingredient.py
@@ -1,11 +1,13 @@
 import csv
 from return_lower_case_string import get_string
+from build_set_food_names import build_set_name
 
 def ask_new_food():
-    new_food_name = get_string()
+    new_food = get_string()
+    set_food_in_list = build_set_name()
+
     
 
-new_food = []
 
 # add new row to csv file from new_food list
 def add_new_ingredient():
@@ -13,4 +15,3 @@ def add_new_ingredient():
         writer = csv.writer(f)
         writer.writerow(new_food)
         f.close()
-print(ask_new_food())
