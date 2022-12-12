8d1f52272474c6160676ae57cc394f5f3ecc93cf
AndresBo <anndresfborrero@gmail.com>
2022-12-12 10:41:53 +1000

    try and failed to implement test for list_foods function

--- test/acceptance_test.py
+++ test/acceptance_test.py
@@ -1,9 +1,11 @@
-# import pytest
-# import sys
+import pytest
+import sys
 
-# sys.path.append('../FRIDGE_APP')
+sys.path.append('../fridge_app')
 
-# from FRIDGE_APP.ingredients import list_foods 
+from ingredients import list_foods
+
+
 
 # def test_list_foods_FileNotFoundError():
 #     with pytest.raises(FileNotFoundError):
