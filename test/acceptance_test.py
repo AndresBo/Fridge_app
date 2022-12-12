2ddb26bc37d60078f8d34c7e05e1335ecae0dacb
AndresBo <anndresfborrero@gmail.com>
2022-12-12 10:28:45 +1000

    trying to build test - can't import from parentdirectory?

---
+++ test/acceptance_test.py
@@ -0,0 +1,10 @@
+# import pytest
+# import sys
+
+# sys.path.append('../FRIDGE_APP')
+
+# from FRIDGE_APP.ingredients import list_foods 
+
+# def test_list_foods_FileNotFoundError():
+#     with pytest.raises(FileNotFoundError):
+#         list_foods()
