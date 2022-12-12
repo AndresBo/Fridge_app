6278667ee05595fc5480e130a853214a57701f5e
AndresBo <anndresfborrero@gmail.com>
2022-12-12 15:00:07 +1000

    adding comments to new_ingredient.py

--- new_ingredient.py
+++ new_ingredient.py
@@ -4,25 +4,25 @@ from build_set_food_names import build_set_name
 from return_integer import get_int
 
 def ask_new_food():
-    new_food = get_string()            
-    set_food_in_list = build_set_name()
-    quantity = 0
-    new_food_list = []
+    new_food = get_string()             #use get_string function to get new food name from user        
+    set_food_in_list = build_set_name() #get a set with current foods
+    quantity = 0                        #create variable to hold quantity of new food
+    new_food_list = []                  #create list to hold name and quatity of new food 
 
-    if new_food in set_food_in_list:
+    if new_food in set_food_in_list:   
         print(f"{new_food} is already in Fridge")
     else:
-        quantity = get_int() 
-        new_food_list.append(new_food)
+        quantity = get_int()           #get new food quantity using get_int function 
+        new_food_list.append(new_food) 
         new_food_list.append(quantity)
         add_new_ingredient(new_food_list)
-        print(f"{quantity} {new_food} have been added")
+        print(f"{quantity} {new_food} have been added") #call add_new_ingredient passing list [name,quantity]
 
 
 
 # add new row to csv file from new_food list
-def add_new_ingredient(new):
+def add_new_ingredient(newList):
     with open('food_list.csv', 'a') as f:
         writer = csv.writer(f)
-        writer.writerow(new)
+        writer.writerow(newList)
         f.close()
