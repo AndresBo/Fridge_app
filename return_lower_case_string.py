752b6a1cb9ac8f9d6d88a69588d51d0f27d49aab
AndresBo <anndresfborrero@gmail.com>
2022-12-12 15:21:02 +1000

    added a quit option to get_string function

--- return_lower_case_string.py
+++ return_lower_case_string.py
@@ -2,13 +2,16 @@ from os import system
 # get a lower case string with leading and trailing whitespace removed
 def get_string():
     while True:
-        new_food = input("Please enter name of new food: ").lower().strip()
-
-        if not new_food.isalpha():
+        
+        new_food = input("Please enter name of new food or to head back to menu enter 'back': ").lower().strip()
+        if new_food == "back":
+            break
+        elif not new_food.isalpha():
             system('clear')
             print("Enter only letters")
+            input("\nPress enter to continue")
             continue
         else:
             return new_food
             break
-    
+#print(get_string()) 
