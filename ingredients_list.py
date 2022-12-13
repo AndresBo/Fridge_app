79c4b7281c2158670f4f0b7cb4e4387ceefc8c24
AndresBo <anndresfborrero@gmail.com>
2022-12-13 15:44:07 +1000

    changed list_food function to take a variable that points to the csv file. to try to implement testing

--- ingredients_list.py
+++ ingredients_list.py
@@ -1,8 +1,8 @@
 import csv
-
+food_list = 'food_list.csv'
 # list of food function using csv module:
 def list_foods():
-    with open('food_list.csv') as f:
+    with open(food_list) as f:
         reader = csv.DictReader(f)
         for row in reader:
             print (f"{row['food']} {row['quantity']}")
--- test/acceptance_test.py
+++ test/acceptance_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../fridge_app')
 
+from ingredients_list import list_foods
 
-
-#test get_string returns lower case string??
+    
