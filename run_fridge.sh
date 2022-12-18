1116d7d333c46be32ef7c18b4dfe5ab225a4a485
AndresBo <anndresfborrero@gmail.com>
2022-12-18 16:02:52 +1000

    added executable bach script

--- run_fridge.sh
+++ run_fridge.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-python3 fridge.py
+
+# Checks for Python 3 then 2 and runs what is available
+
+if command -v python3 >/dev/null 2>&1; then
+  python3 fridge.py
+else
+  if command -v python >/dev/null 2>&1; then
+    python fridge.py
+  else
+    echo "Error: Python 3 or Python 2 is required to run this script"
+  fi
+fi
